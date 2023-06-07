9f410d7c669c4203da03f43db8dc60fdc2829634
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2023-06-07 15:49:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ CONDA_DIR='/var/lib/miniconda3'
 
 # Install pytorch and biom3d
 ss-display "Installing Pytorch and biom3d (with pip3)"
-python3 -m pip install torch biom3d
+sudo apt-get install python3-tk
+python3 -m pip install torch omero-py biom3d
