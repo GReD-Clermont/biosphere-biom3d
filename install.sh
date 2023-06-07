cd6715667a6e8d172c41978410727a78db0b2f5c
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2023-06-07 17:27:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ source /etc/profile
 LOCUSER='ubuntu'
 CONDA_DIR='/var/lib/miniconda3' 
 
+# Install x2go
+sudo apt-get install x2goserver x2goserver-xsession xfce4-session
+
 # Install pytorch and biom3d
 ss-display "Installing Pytorch and biom3d (with pip3)"
-sudo apt-get install python3-tk
-python3 -m pip install torch omero-py biom3d
+sudo apt-get install python3-tk python3-dev libssl-dev
+python3 -m pip install torch omero-py biom3d biom3d[gui]
