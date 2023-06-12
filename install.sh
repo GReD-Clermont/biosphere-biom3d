42a9af812a48feb66c22d5a483c599f3aca595c7
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2023-06-12 16:42:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ LOCUSER='ubuntu'
 CONDA_DIR='/var/lib/miniconda3' 
 
 # Install x2go
-sudo apt-get install x2goserver x2goserver-xsession xfce4-session
+# sudo apt-get install x2goserver x2goserver-xsession xfce4-session
 
 # Install pytorch and biom3d
 ss-display "Installing Pytorch and biom3d (with pip3)"
-sudo apt-get install python3-tk python3-dev libssl-dev libbz2-dev
+sudo apt-get install python3-tk python3-dev libssl-dev
+python3 -m pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
 python3 -m pip install torch omero-py biom3d biom3d[gui]
