9bad5f62e4768eef4ddadc537ec5393dd2c7a3b5
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2023-06-13 16:30:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ ss-display "Installing Pytorch and biom3d (with pip3)"
 sudo apt-get install -y python3-tk python3-dev libssl-dev
 python3 -m pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
 python3 -m pip install torch omero-py 
-python3 -m pip install --force-reinstall PyYAML==5.4
-python3 -m pip install biom3d biom3d[gui]
+python3 -m pip install tqdm scikit-image scipy SimpleITK pandas tensorboard protobuf numpy matplotlib appdirs numba torchio paramiko netcat
+python3 -m pip install --no-deps biom3d biom3d[gui]
