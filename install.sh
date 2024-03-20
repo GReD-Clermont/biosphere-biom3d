815bb0a994e38c0ffbc190d5416286c18e452432
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2024-03-20 15:46:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ source /etc/profile
 LOCUSER='ubuntu'
 CONDA_DIR='/var/lib/miniconda3' 
 
+# Python is python3
+sudo apt-get install python-is-python3
+
 # Install pytorch and biom3d
 ss-display "Installing Pytorch and biom3d (with pip3)"
 sudo apt-get install -y python3-tk python3-dev libssl-dev
