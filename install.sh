cda30110b7379869b9bb38e0732f5792e0542686
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2024-03-27 11:13:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ CONDA_DIR='/var/lib/miniconda3'
 
 # Install pytorch and biom3d
 ss-display "Installing Pytorch and biom3d (with pip3)"
-sudo apt-get install -y python3-tk python3-dev libssl-dev
-python3 -m pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
-python3 -m pip install torch omero-py 
-
-# Python is python3
+sudo apt-get update
+sudo apt-get install -y python3-tk python3-dev libssl-dev python3-pip
 sudo apt-get install -y python-is-python3
+pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
+pip install torch omero-py 
 
 # Install biom3d dependencies independently because PyYAML dependency cannot be re-installed on the existing Appliance 
-python3 -m pip install tqdm scikit-image scipy SimpleITK pandas tensorboard protobuf numpy matplotlib appdirs numba torchio paramiko netcat
-python3 -m pip install --no-deps biom3d biom3d[gui]
+pip install tqdm scikit-image scipy SimpleITK pandas tensorboard protobuf numpy matplotlib appdirs numba torchio paramiko netcat
+pip install --no-deps biom3d biom3d[gui]
 
 # Install napari
 sudo apt-get install -y qt5-style-plugins
-python3 -m pip install "napari[all]"
+pip install "napari[all]"
