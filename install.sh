a6390df5d7035e529e8d14190e40d2bb3f989b82
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2024-03-27 11:36:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ CONDA_DIR='/var/lib/miniconda3'
 ss-display "Installing Pytorch and biom3d (with pip3)"
 sudo apt-get update
 sudo apt-get install -y python3-tk python3-dev libssl-dev python3-pip
+python3 -m pip install pip --upgrade
 sudo apt-get install -y python-is-python3
+pip install pyopenssl --upgrade
 pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/download/0.2.0/zeroc_ice-3.6.5-cp38-cp38-linux_x86_64.whl
 pip install torch omero-py 
 
