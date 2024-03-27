15829d33b06ecf94d3aeb771396ad4b1572e5dee
Guillaume Mougeot <guillaume.mougeot@laposte.net>
2024-03-27 17:13:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,6 @@ python3 -m pip install https://github.com/ome/zeroc-ice-ubuntu2004/releases/down
 python3 -m pip install torch omero-py 
 python3 -m pip install tqdm scikit-image scipy SimpleITK pandas tensorboard protobuf numpy matplotlib appdirs numba torchio paramiko netcat
 python3 -m pip install --no-deps biom3d biom3d[gui]
+
+sudo apt-get install -y qt5-style-plugins
+python3 -m pip install "napari[all]"
